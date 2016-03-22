8f91f1c9f40b8246f286fc0b3a47c96933d7e557
stern15 <ssemasuka@gmail.com>
2016-03-22 15:57:00 +0300

    getting the users number and displaying to the android log

--- app/src/main/java/com/semasuka/numbershape/MainActivity.java
+++ app/src/main/java/com/semasuka/numbershape/MainActivity.java
@@ -2,10 +2,17 @@ package com.semasuka.numbershape;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public void testNumber(View view){
+
+        EditText usersNumber=(EditText)findViewById(R.id.usersNumber);
+
+        Log.i("userNumber",usersNumber.getText().toString());
+
 
 
     }
