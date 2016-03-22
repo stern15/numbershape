79ab0c7e07034052cda8a6f7c9427b90ec694efb
stern15 <ssemasuka@gmail.com>
2016-03-22 16:33:30 +0300

    adding the method to calculate the triangular and square number of the app

--- app/src/main/java/com/semasuka/numbershape/MainActivity.java
+++ app/src/main/java/com/semasuka/numbershape/MainActivity.java
@@ -7,11 +7,60 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
+    class Number{
+        int Number;
+        public boolean isSquare(){
+            int x=1;
+            int Square=1;
+            //this will generated all the square numbers up to our number
+            while(Square<Number){
+                x++;
+                Square= x*x;
+            }
+            //if our number equal to the
+            if(Square==Number){
+                return true;
+            }
+            else{
+                return false;
+            }
+
+        }
+        public boolean isTriangular(){
+            int x=1;
+            int triangularNumber=1;
+
+            while(triangularNumber < Number){
+                x++;
+                triangularNumber=triangularNumber+x;
+            }
+            if(triangularNumber==Number){
+                return true;
+            }
+            else{
+                return false;
+            }
+
+        }
+
+    }
+
     public void testNumber(View view){
 
         EditText usersNumber=(EditText)findViewById(R.id.usersNumber);
 
         Log.i("userNumber",usersNumber.getText().toString());
+
+
+        Number myNumber=new Number();
+        myNumber.Number=Integer.parseInt(usersNumber.getText().toString());
+        System.out.println("Is "+myNumber.Number+" a square number?Answer:"+myNumber.isSquare());
+        System.out.println("Is " + myNumber.Number + " a triangular number?Answer:" + myNumber.isTriangular());
+
 
 
 
