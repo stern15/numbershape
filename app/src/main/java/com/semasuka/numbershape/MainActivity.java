a9a9bdec95f883e98f5dd13a58dcd2263c5071cb
stern15 <ssemasuka@gmail.com>
2016-03-22 15:53:10 +0300

    adding the method that will hold everything in the main activity

--- app/src/main/java/com/semasuka/numbershape/MainActivity.java
+++ app/src/main/java/com/semasuka/numbershape/MainActivity.java
@@ -1,9 +1,14 @@
 package com.semasuka.numbershape;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public void testNumber(View view){
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
