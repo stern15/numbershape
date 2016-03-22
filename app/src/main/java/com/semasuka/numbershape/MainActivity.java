36636f522bd9147f312fc8bb4d73b5a2170bca3e
stern15 <ssemasuka@gmail.com>
2016-03-22 16:35:18 +0300

    removed log

--- app/src/main/java/com/semasuka/numbershape/MainActivity.java
+++ app/src/main/java/com/semasuka/numbershape/MainActivity.java
@@ -2,7 +2,6 @@ package com.semasuka.numbershape;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -53,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
         EditText usersNumber=(EditText)findViewById(R.id.usersNumber);
 
-        Log.i("userNumber",usersNumber.getText().toString());
+
 
 
         Number myNumber=new Number();
